121827c74a6be2a716e6e16228ec654636985696
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2020-11-21 16:05:15 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,12 +14,35 @@ const menu = document.querySelector('.menu-burger'),
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
-    menuBody.classList.toggle('active');
+	menuBody.classList.toggle('active');
+	if (menuBody.classList.contains('active')) {
+		document.body.style.overflowY = 'hidden';
+	} else {
+		document.body.style.overflowY = 'visible';
+	}
 });
 
-const paralaxAreas = document.querySelectorAll('.paralax-area');
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
 
 let isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
+
+const paralaxAreas = document.querySelectorAll('.paralax-area');
 
 if (!isMobile.any()) {
 	if (paralaxAreas.length > 0) {
